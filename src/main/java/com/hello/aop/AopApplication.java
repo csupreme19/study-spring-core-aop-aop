8e1d45fb6ef824c01dd27dc7d53cdc0e28e4afc0
최승훈 (AI Industry플랫폼개발TF) <seunghoon.choi@kt.com>
2022-08-02 18:41:49 +0900

    Spring AOP init

--- src/main/java/com/hello/aop/AopApplication.java
+++ src/main/java/com/hello/aop/AopApplication.java
@@ -2,7 +2,9 @@ package com.hello.aop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+// @EnableAspectJAutoProxy Spring Boot에서는 자동으로 설정해준다.
 @SpringBootApplication
 public class AopApplication {
 
