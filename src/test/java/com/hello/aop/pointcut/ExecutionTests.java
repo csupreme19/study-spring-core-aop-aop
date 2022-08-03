c46d39deaa98be111756ebada10db70222a8b795
최승훈 (AI Industry플랫폼개발TF) <seunghoon.choi@kt.com>
2022-08-03 18:47:54 +0900

    PCD execution 지시자 모두 써보기

--- src/test/java/com/hello/aop/pointcut/ExecutionTests.java
+++ src/test/java/com/hello/aop/pointcut/ExecutionTests.java
@@ -16,10 +16,12 @@ public class ExecutionTests {
     AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
 
     Method helloMethod;
+    Method internalMethod;
 
     @BeforeEach
     void setUp() throws NoSuchMethodException {
         helloMethod = MemberServiceImpl.class.getMethod("hello", String.class);
+        internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
     }
 
     @Test
@@ -113,8 +115,99 @@ public class ExecutionTests {
     void packageSubPackage2MatchTest() {
         // PCD Pointcut Designator
         // 접근제어자? 반환타입   선언타입?         메서드이름 파라미터 예외?
-        //            *     com.hello.aop...*.*   (..)
+        //            *     com.hello..*.*           (..)
         pointcut.setExpression("execution(* com.hello..*.*(..))");  // 하위 패키지까지 포함하려면 .. 사용
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void typeExactMatchTest() {
+        // PCD Pointcut Designator
+        // 접근제어자? 반환타입   선언타입?                              메서드이름 파라미터 예외?
+        //            *     com.hello.aop.member.MemberServiceImpl.*     (..)
+        pointcut.setExpression("execution(* com.hello.aop.member.MemberServiceImpl.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void typeSuperTypeMatchTest() {
+        // PCD Pointcut Designator
+        // 접근제어자? 반환타입   선언타입?                         메서드이름 파라미터 예외?
+        //            *     com.hello.aop.member.MemberService.*     (..)
+        pointcut.setExpression("execution(* com.hello.aop.member.MemberService.*(..))");    // 타입 매칭 - 상위 클래스/인터페이스를 지정하더라도 자식 클래스의 메서드가 매칭됨!
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void typeInternalMatchTest() {
+        // PCD Pointcut Designator
+        // 접근제어자? 반환타입   선언타입?                              메서드이름 파라미터 예외?
+        //            *     com.hello.aop.member.MemberServiceImpl.*     (..)
+        pointcut.setExpression("execution(* com.hello.aop.member.MemberServiceImpl.*(..))");    // 타입 매칭 - 부모 타입에 정의되어 있는 메서드에 한정된다.
+        assertThat(pointcut.matches(internalMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void typeInternalMatchFailTest() {
+        // PCD Pointcut Designator
+        // 접근제어자? 반환타입   선언타입?                              메서드이름 파라미터 예외?
+        //            *     com.hello.aop.member.MemberService.     *     (..)
+        pointcut.setExpression("execution(* com.hello.aop.member.MemberService.*(..))");    // 타입 매칭 - 부모 타입에 정의되어 있는 메서드에 한정된다.
+        assertThat(pointcut.matches(internalMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    void parameterMatchTest() {
+        // PCD Pointcut Designator
+        // 접근제어자? 반환타입 선언타입? 메서드이름 파라미터 예외?
+        //            *             *   (String)
+        pointcut.setExpression("execution(* *(String))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+        assertThat(pointcut.matches(internalMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void argsMatchNoArgsTest() {
+        // PCD Pointcut Designator
+        // 접근제어자? 반환타입 선언타입? 메서드이름 파라미터 예외?
+        //            *             *       ()
+        pointcut.setExpression("execution(* *())"); // 파라미터가 없는 메서드
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    void argsMatchOneArgTest() {
+        // PCD Pointcut Designator
+        // 접근제어자? 반환타입 선언타입? 메서드이름 파라미터 예외?
+        //            *             *       (*)
+        pointcut.setExpression("execution(* *(*))");    // 파라미터가 단 하나만 존재하는 메서드
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void argsMatchTwoArgsTest() {
+        // PCD Pointcut Designator
+        // 접근제어자? 반환타입 선언타입? 메서드이름 파라미터 예외?
+        //            *             *       (*, *)
+        pointcut.setExpression("execution(* *(*, *))");    // 파라미터가 딱 두 개만 존재하는 메서드
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    @Test
+    void argsMatchAllArgsTest() {
+        // PCD Pointcut Designator
+        // 접근제어자? 반환타입 선언타입? 메서드이름 파라미터 예외?
+        //            *             *       (..)
+        pointcut.setExpression("execution(* *(..))");   // 모든 파라미터
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void argsMatchStartsWithStringArgsTest() {
+        // PCD Pointcut Designator
+        // 접근제어자? 반환타입 선언타입? 메서드이름 파라미터 예외?
+        //            *             *       (String, ..)
+        pointcut.setExpression("execution(* *(String, ..))");   // 첫번째 인자가 String인 모든 파라미터
         assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
 }
