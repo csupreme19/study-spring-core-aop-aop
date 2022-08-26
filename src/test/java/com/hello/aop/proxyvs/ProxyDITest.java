0f6c2bb6df765d6376d300c2260f75e18a7faccf
최승훈 (AI Industry플랫폼개발TF) <seunghoon.choi@kt.com>
2022-08-26 17:30:19 +0900

    스프링 AOP에서 문제를 해결한 방법

--- src/test/java/com/hello/aop/proxyvs/ProxyDITest.java
+++ src/test/java/com/hello/aop/proxyvs/ProxyDITest.java
@@ -20,6 +20,14 @@ class ProxyDITest {
     @Autowired
     MemberServiceImpl memberServiceImpl;
 
+    /**
+     * Spring 3.2부터 CGLIB를 기본 프록시 라이브러리로 제공
+     * 문제점: 생성자가 2번 호출된다.
+     * 1. target의 생성자 호출 1번(target 객체가 생성될 때)
+     * 2. proxy의 생성자 호출 1번(proxy 객체 부모 생성자 = target 생성자)
+     * 이유: proxy의 부모 클래스는 target이므로 부모 클래스의 생성자도 동일하게 호출
+     * Spring 4.0부터 objenesis 라이브러리를 사용하여 생성자 호출 없이 객체 생성이 가능하므로 생성자 2번 호출의 문제점이 해결
+     */
     @Test
     void doTrace() {
         log.info("memberService class={}", memberService.getClass());
